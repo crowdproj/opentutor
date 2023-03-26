47a7169edcc63c9307a002fbf8069b560887e393
sszuev <sss.zuev@gmail.com>
2023-03-26 17:40:08 +0300

    flashcards-kt: update dependencies (ktor -> 2.2.4, caffeine -> 3.1.5, postgres -> 42.6.0, liquibase -> 4.20.0, jquery -> 3.6.3, commons-compress -> 1.23.0, logback -> 1.4.6, slf4j -> 2.0.7, rabbit-mq -> 5.17.0, docker-java-application -> 9.3.0) + fix gradle compile issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,15 @@ allprojects {
     }
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    tasks.withType<JavaCompile> {
+        targetCompatibility = "11"
+    }
 }
