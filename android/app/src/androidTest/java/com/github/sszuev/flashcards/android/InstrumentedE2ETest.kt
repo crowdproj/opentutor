51a4a967c4e2ff2e52e7acc8ea1883fb4257dcf7
sszuev <sss.zuev@gmail.com>
2025-03-15 19:02:03 +0300

    android: add testDeleteDictionary & testAddCard

--- android/app/src/androidTest/java/com/github/sszuev/flashcards/android/InstrumentedE2ETest.kt
+++ android/app/src/androidTest/java/com/github/sszuev/flashcards/android/InstrumentedE2ETest.kt
@@ -15,6 +15,7 @@ import androidx.test.uiautomator.By
 import androidx.test.uiautomator.BySelector
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
+import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.FixMethodOrder
@@ -44,6 +45,11 @@ class InstrumentedE2ETest {
     fun test() {
         testSignIn()
         testCreateDictionary()
+        try {
+            testAddCards()
+        } finally {
+            testDeleteDictionary()
+        }
     }
 
     private fun testSignIn() {
@@ -108,7 +114,7 @@ class InstrumentedE2ETest {
 
         val passwdSelector = By.res("password")
         if (!device.hasObject(passwdSelector)) {
-            val foundAfterScroll = device.scrollUntilVisible(passwdSelector, maxSwipes = 5)
+            val foundAfterScroll = device.scrollUntilVisible(targetSelector = passwdSelector, maxSwipes = 5)
             if (!foundAfterScroll) {
                 throw AssertionError("Can't find password text-box")
             }
@@ -118,7 +124,7 @@ class InstrumentedE2ETest {
 
         val loginSelector = By.res("kc-login")
         if (!device.hasObject(loginSelector)) {
-            val foundAfterScroll = device.scrollUntilVisible(loginSelector, maxSwipes = 5)
+            val foundAfterScroll = device.scrollUntilVisible(targetSelector = loginSelector, maxSwipes = 5)
             if (!foundAfterScroll) {
                 throw AssertionError("Can't find log-in button")
             }
@@ -139,44 +145,122 @@ class InstrumentedE2ETest {
     }
 
     private fun testCreateDictionary() {
+        Log.i(tag, "CREATE DICTIONARY $testDictionaryName")
         val foundCreate = device.wait(Until.hasObject(By.text("CREATE")), 5_000)
         assertTrue("CREATE button not found", foundCreate)
         Log.i(tag, "Click CREATE button")
         device.findObject(By.text("CREATE")).click()
 
-        device.wait(Until.hasObject(By.desc("SelectField1")), 3_000)
+        device.wait(Until.hasObject(By.desc("SelectField1")), 5_000)
         device.findObject(By.desc("SelectField1")).click()
 
-        device.wait(Until.hasObject(By.desc("SearchField1")), 3_000)
+        device.wait(Until.hasObject(By.desc("SearchField1")), 5_000)
         device.findObject(By.desc("SearchField1")).click()
 
-        device.pressKeyCode(KeyEvent.KEYCODE_E)
-        device.pressKeyCode(KeyEvent.KEYCODE_N)
+        device.type("en")
 
-        device.wait(Until.hasObject(By.text("English")), 3_000)
+        device.wait(Until.hasObject(By.text("English")), 5_000)
         device.findObject(By.text("English")).click()
 
-        device.wait(Until.hasObject(By.desc("SelectField2")), 3_000)
+        device.wait(Until.hasObject(By.desc("SelectField2")), 5_000)
         device.findObject(By.desc("SelectField2")).click()
 
-        device.wait(Until.hasObject(By.desc("SearchField2")), 3_000)
+        device.wait(Until.hasObject(By.desc("SearchField2")), 5_000)
         device.findObject(By.desc("SearchField2")).click()
 
-        device.pressKeyCode(KeyEvent.KEYCODE_R)
-        device.pressKeyCode(KeyEvent.KEYCODE_U)
+        device.type("ru")
 
-        device.wait(Until.hasObject(By.text("Russian (русский)")), 3_000)
+        device.wait(Until.hasObject(By.text("Russian (русский)")), 5_000)
         device.findObject(By.text("Russian (русский)")).click()
 
-        device.wait(Until.hasObject(By.desc("DictionaryName")), 3_000)
+        device.wait(Until.hasObject(By.desc("DictionaryName")), 5_000)
         device.findObject(By.desc("DictionaryName")).text = testDictionaryName
 
-        device.wait(Until.hasObject(By.desc("AcceptedAnswers")), 3_000)
+        device.wait(Until.hasObject(By.desc("AcceptedAnswers")), 5_000)
         device.findObject(By.desc("AcceptedAnswers")).text = "14"
 
-        device.wait(Until.hasObject(By.text("SAVE")), 3_000)
+        device.wait(Until.hasObject(By.text("SAVE")), 5_000)
         Log.i(tag, "Click SAVE button")
         device.findObject(By.text("SAVE")).click()
+
+        val found = device.wait(Until.hasObject(By.text(testDictionaryName)), 5_000)
+        assertTrue("New dictionary not found in the list", found)
+    }
+
+    private fun testDeleteDictionary() {
+        Log.i(tag, "DELETE DICTIONARY $testDictionaryName")
+        device.pressBack()
+        val foundHome = device.wait(Until.hasObject(By.text("HOME")), 1_000)
+        if (foundHome) {
+            device.findObject(By.text("HOME")).click()
+        } else {
+            device.pressBack()
+            device.wait(Until.hasObject(By.text("HOME")), 5_000)
+            device.findObject(By.text("HOME")).click()
+        }
+
+        val dictionarySelector = By.text(testDictionaryName)
+        device.wait(Until.hasObject(dictionarySelector), 5_000)
+        if (!device.hasObject(dictionarySelector)) {
+            val foundAfterScroll = device.scrollUntilVisible(
+                targetSelector = dictionarySelector,
+                maxSwipes = 5,
+                fromTopToBottom = false,
+            )
+            if (!foundAfterScroll) {
+                throw AssertionError("Can't find dictionary $testDictionaryName")
+            }
+        }
+        device.findObject(dictionarySelector).click()
+        device.waitForIdle(500)
+
+        device.wait(Until.hasObject(By.text("DELETE")), 5_000)
+        Log.i(tag, "Click DELETE button")
+        device.findObject(By.text("DELETE")).click()
+
+        val foundConfirm = device.wait(Until.hasObject(By.text("CONFIRM")), 5_000)
+        if (foundConfirm) {
+            device.findObject(By.text("CONFIRM")).click()
+        } else {
+            device.findObject(By.text("DELETE")).click()
+            device.wait(Until.hasObject(By.text("CONFIRM")), 5_000)
+            device.findObject(By.text("CONFIRM")).click()
+        }
+
+        device.wait(Until.gone(By.text(testDictionaryName)), 5_000)
+        Assert.assertFalse(device.hasObject(By.text(testDictionaryName)))
+    }
+
+    private fun testAddCards() {
+        device.wait(Until.hasObject(By.text(testDictionaryName)), 5_000)
+        device.wait(Until.hasObject(By.text("CARDS")), 5_000)
+        Log.i(tag, "Click CARDS button")
+        device.findObject(By.text("CARDS")).click()
+
+        device.wait(Until.hasObject(By.desc("CardWordName")), 5_000)
+        device.findObject(By.desc("CardWordName")).click()
+
+        device.type("weather")
+
+        device.wait(Until.hasObject(By.text("ADD")), 5_000)
+        device.findObject(By.text("ADD")).click()
+
+        device.wait(Until.hasObject(By.desc("AddDialogWord")), 5_000)
+        device.wait(Until.hasObject(By.desc("AddDialogTranslation")), 5_000)
+        device.findObject(By.desc("AddDialogTranslation")).click()
+        device.clearTextField()
+        device.type("pogoda")
+        device.pressBack()
+
+        val saveSelector = By.text("SAVE")
+        val foundSave = device.wait(Until.hasObject(saveSelector), 1_000)
+        if (foundSave) {
+            device.findObject(saveSelector).click()
+        } else {
+            device.scrollUntilVisible(saveSelector)
+            device.wait(Until.hasObject(saveSelector), 5_000)
+            device.findObject(saveSelector).click()
+        }
     }
 
     private fun trySignOutIfAvailable() {
@@ -192,25 +276,62 @@ class InstrumentedE2ETest {
 
     private fun UiDevice.scrollUntilVisible(
         targetSelector: BySelector,
-        maxSwipes: Int = 5
+        maxSwipes: Int = 5,
+        fromTopToBottom: Boolean = true,
     ): Boolean {
         val screenWidth = this.displayWidth
         val screenHeight = this.displayHeight
 
-        repeat(maxSwipes) { attempt ->
+        repeat(maxSwipes) { _ ->
             if (this.hasObject(targetSelector)) {
                 return true
             }
-            swipe(
-                screenWidth / 2,   // fromX
-                (screenHeight * 3) / 4,  // fromY
-                screenWidth / 2,   // toX
-                screenHeight / 4,  // toY
-                20
-            )
+            if (fromTopToBottom) {
+                swipe(
+                    /* startX = */ screenWidth / 2,   // fromX
+                    /* startY = */ (screenHeight * 3) / 4,  // fromY
+                    /* endX = */ screenWidth / 2,   // toX
+                    /* endY = */ screenHeight / 4,  // toY
+                    /* steps = */ 20
+                )
+            } else {
+                swipe(
+                    /* startX = */ screenWidth / 2,
+                    /* startY = */ screenHeight / 4,
+                    /* endX = */ screenWidth / 2,
+                    /* endY = */ (screenHeight * 3) / 4,
+                    /* steps = */ 20
+                )
+            }
             Thread.sleep(500)
         }
         return this.hasObject(targetSelector)
+    }
+
+    private fun UiDevice.clearTextField(times: Int = 50) {
+        repeat(times) {
+            pressKeyCode(KeyEvent.KEYCODE_DEL)
+            Thread.sleep(50)
+        }
+    }
+
+    private fun UiDevice.type(word: String) {
+        for (char in word) {
+            if (char.isLetter()) {
+                val lowerChar = char.lowercaseChar()
+                val keyCode = KeyEvent.KEYCODE_A + (lowerChar - 'a')
+                if (char.isUpperCase()) {
+                    pressKeyCode(KeyEvent.KEYCODE_SHIFT_LEFT)
+                }
+                pressKeyCode(keyCode)
+            } else if (char.isDigit()) {
+                val keyCode = KeyEvent.KEYCODE_0 + (char - '0')
+                pressKeyCode(keyCode)
+            } else if (char == ' ') {
+                pressKeyCode(KeyEvent.KEYCODE_SPACE)
+            }
+            Thread.sleep(100)
+        }
     }
 
     companion object TestData {
