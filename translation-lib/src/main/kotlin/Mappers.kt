2529078fbf7d3f35fd12a5cf7593bf21602cf92e
sszuev <sss.zuev@gmail.com>
2025-04-27 21:00:43 +0300

    translation: remove support for linguee-api

--- translation-lib/src/main/kotlin/Mappers.kt
+++ translation-lib/src/main/kotlin/Mappers.kt
@@ -1,22 +1,6 @@
 package com.gitlab.sszuev.flashcards.translation.impl
 
 import com.gitlab.sszuev.flashcards.translation.api.TranslationEntity
-import com.gitlab.sszuev.flashcards.translation.api.TranslationExample
-
-internal fun LingueeEntry.toTWord() = TranslationEntity(
-    partOfSpeech = this.pos,
-    word = this.text,
-    translations = this.translations.toTTranslations(),
-    examples = this.translations.flatMap { it.toTExamples() }
-)
-
-private fun List<LingueeEntryTranslation>.toTTranslations(): List<List<String>> {
-    return listOf(filterNot { it.text.isNullOrBlank() }.map { checkNotNull(it.text) })
-}
-
-private fun LingueeEntryTranslation.toTExamples() = (this.examples ?: emptyList()).map { it.toTExample() }
-
-private fun LingueeEntryExample.toTExample() = TranslationExample(text = src, translation = dst)
 
 internal fun YandexEntry.toTWords() = this.def.map { it.toTEntity() }
 
