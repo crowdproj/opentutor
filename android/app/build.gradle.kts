fdce556249d25d53726cd222ffbce7cb5c5e6ef1
sszuev <sss.zuev@gmail.com>
2025-06-07 18:42:55 +0300

    android: next version 3 (1.2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.sszuev.flashcards.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.github.sszuev.flashcards.android"
